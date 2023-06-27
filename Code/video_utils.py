35d8e1d4e320d3486331fbfd5a8ffa65d6707ecc
asafweinerg <asafweinber@mail.tau.ac.il.com>
2023-06-27 15:56:42 +0300

    utils

---
+++ Code/video_utils.py
@@ -0,0 +1,24 @@
+import cv2
+
+def get_video_parameters(capture: cv2.VideoCapture) -> dict:
+    """Get an OpenCV capture object and extract its parameters.
+
+    Args:
+        capture: cv2.VideoCapture object.
+
+    Returns:
+        parameters: dict. Video parameters extracted from the video.
+
+    """
+    fourcc = int(capture.get(cv2.CAP_PROP_FOURCC))
+    fps = int(capture.get(cv2.CAP_PROP_FPS))
+    height = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    width = int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    frame_count = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+    return {"fourcc": fourcc, "fps": fps, "height": height, "width": width,
+            "frame_count": frame_count}
+
+
+def init_vid_writer(output_path: str, params, isColor):
+    out_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'XVID'), params['fps'], (params['width'], params['height']), isColor)
+    return out_writer
